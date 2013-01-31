aa4aa3d9c1351d8251ef8277a749eae652efde13
Michael Nye <mnye@andrew.cmu.edu>
2013-01-31 18:17:06 -0500

    Refactored test code into library, added documentation

---
+++ portaudio_utils.h
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "portaudio.h"
+
+
+#ifndef AUDIOCONSTS
+#define AUDIOCONSTS
+
+/* Define the nessecary constants for portaudio sampling.
+ * Per assignment specs, uses 16-bit integers for each sample. */
+typedef short SAMPLE;
+#define PA_SAMPLE_TYPE  paInt16
+#define SAMPLE_SILENCE  (0)
+#define PRINTF_S_FORMAT "%d"
+
+/* Define the recording constants for our streams. Try to use these constants
+ * everywhere when possible. */
+#define SAMPLE_RATE        (16000)
+#define NUM_CHANNELS       (1)
+#define SAMPLES_PER_BUFFER (SAMPLE_RATE)
+
+#endif
+
+
+/* error_check - takes a string describing the function called, and an
+ *               error code denoting that function. If the function
+ *               worked successfully, it just returns. Otherwise, it prints out
+ *               the location and the error description, then exits().
+ *
+ *               Currently returns 0.
+ *
+ * Sample usage: error_check("Pa_MyFunc", Pa_MyFunc(args));
+ */
+int error_check(char* location, int err);
+
+
+/* initialize_portaudio - given a pointer to store the stream data to, performs
+ *                        all nessecary initialization to use portaudio streams.
+ *                        
+ *                        Currently returns 0
+ */
+int initialize_portaudio(PaStream** stream);
+
+
+/* close_portaudio - given an initialized stream pointer, performs the nessecary
+ *                   procedures to shut down portaudio.
+ *
+ *                   Currently returns 0;
+ */
+int close_portaudio(PaStream** stream);
--- test.c
+++ test.c
@@ -1,72 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include "portaudio.h"
-
-// Need this?
-#define CHECK_OVERFLOW  (0)
-#define CHECK_UNDERFLOW  (0)
-
-// Define sample format
-typedef short SAMPLE;
-#define PA_SAMPLE_TYPE  paInt16
-#define SAMPLE_SILENCE  (0)
-#define PRINTF_S_FORMAT "%d"
-
-// Define recording constants
-#define SAMPLE_RATE        (16000)
-#define SAMPLES_PER_BUFFER (SAMPLE_RATE)
-#define NUM_CHANNELS       (1)
-#define NUM_SECONDS        (10)
+#include "portaudio_utils.h"
 
 
+/* test.c - main function for our working test of portaudio import
+ *      TODO: rename and continue refactoring code
+ *
+ *      Currently performs a basic push-to-begin recording of user input, with
+ *      a windowing function to determine when to stop audio. Writes the raw
+ *      recorded data to a file recorded.out. */
 int main()
 {
-    //Define portaudio magic
-    PaStreamParameters inputParams, outputParams;
+    //Initialize portaudio stream
     PaStream* stream = NULL;
-    PaError err;
-    SAMPLE* samples = malloc(NUM_CHANNELS * SAMPLE_RATE * NUM_SECONDS * sizeof(SAMPLE));
-
-    //Initialize portaudio
-    err = Pa_Initialize();
-
-    //Set up stream params
-    inputParams.device = Pa_GetDefaultInputDevice();
-    inputParams.channelCount = 1;
-    inputParams.sampleFormat = PA_SAMPLE_TYPE;
-    inputParams.suggestedLatency =
-        Pa_GetDeviceInfo(inputParams.device)->defaultHighInputLatency;
-    inputParams.hostApiSpecificStreamInfo = NULL;
-
-    outputParams.device = Pa_GetDefaultOutputDevice();
-    outputParams.channelCount = 1;
-    outputParams.sampleFormat = PA_SAMPLE_TYPE;
-    outputParams.suggestedLatency =
-        Pa_GetDeviceInfo(outputParams.device)->defaultHighOutputLatency;
-    outputParams.hostApiSpecificStreamInfo = NULL;
-
-    printf("Using input device #%d and output device #%d.\n",
-           inputParams.device, outputParams.device);
-
-    //Open the stream!
-    err = Pa_OpenStream(&stream, &inputParams, &outputParams,
-                        SAMPLE_RATE, SAMPLES_PER_BUFFER, paClipOff,
-                        NULL, NULL);
+    initialize_portaudio(&stream);
     
+    //Create buffer to read audio into
+    SAMPLE* samples = malloc(NUM_CHANNELS * SAMPLES_PER_BUFFER * sizeof(SAMPLE));
+
+
+    //Wait for the user to begin
     printf("\nPress enter to begin recording...\n");
     getchar();
-    int outputfile = open("./recorded.out", O_WRONLY|O_CREAT, 0644);
-    int listening = 1;
-    int counter = 0;
 
-    //Run until user ends it!
-    err = Pa_StartStream(stream);
+    //Run until windowing function signals the end, ignoring the
+    //  first sample to let the user begin talking
+    //Write the recorded data to an output file
+    error_check("StartStream",Pa_StartStream(stream));
+    int outputfile = open("./recorded.out", O_WRONLY|O_CREAT, 0644);
+
+    bool listening = true;
+    int counter = 0;
     while(listening || counter < 2)
     {
-        err = Pa_ReadStream(stream, samples, SAMPLES_PER_BUFFER);
-        //err = Pa_WriteStream(stream, samples, SAMPLES_PER_BUFFER);
+        Pa_ReadStream(stream, samples, SAMPLES_PER_BUFFER);
         write(outputfile, samples, SAMPLES_PER_BUFFER);
 
         //Window - stop recording when the magnitude averaged over a
@@ -77,16 +48,13 @@ int main()
             sum += abs(samples[i]);
         }
         int average = sum/SAMPLES_PER_BUFFER;
-        if(average < 250) listening = 0;
+        if(average < 250) listening = false;
         counter++;
     }
-    //No escape yet
 
-    printf("You are done!\n");
-    err = Pa_StopStream(stream);
-    err = Pa_CloseStream(stream);
-    err = Pa_Terminate();
-
+    //Close portaudio and clean up   
+    error_check("StopStream", Pa_StopStream(stream));
+    close_portaudio(stream);
     close(outputfile);
     free(samples);
 
