2edbb928f917abb6bcb17af1f2117c32639e7bb9
Ben <gilmanb33@gmail.com>
2013-02-03 13:18:24 -0500

    Heard Sound Recorded for the First Time!
    Going to clean up the code now.

--- wav_utils.c
+++ wav_utils.c
@@ -33,6 +33,7 @@ int raw_to_wav(char *infile_name, char *outfile_name, int data_length) {
 void make_header(char* header, int data_length) {
 
 	/* make a wav header based on the information passed into the function */
+	/*
 	memset(header, RIFF_ID, 4);
 	memset(header+4, CHUNK_SIZE, 4);
 	memset(header+8, WAVE_ID, 4);
@@ -40,14 +41,15 @@ void make_header(char* header, int data_length) {
 	memset(header+16, SUBCHUNK_1_SIZE, 4);
 	memset(header+20, AUDIO_FORMAT, 2);
 	memset(header+22, HEADER_NUM_CHANNELS, 2);
-
+	*/
+	
 	//Chunk Descriptor
 	(header)[0] = 0x52;
 	(header)[1] = 0x49;
 	(header)[2] = 0x46;
 	(header)[3] = 0x46;
 	
-	//ChunkSize
+	//ChunkSize: 2084
 	(header)[4] = 0x24;
 	(header)[5] = 0x08;
 	(header)[6] = 0x00;
@@ -65,55 +67,53 @@ void make_header(char* header, int data_length) {
 	(header)[14] = 0x74;
 	(header)[15] = 0x20;
 	
-	//Subchunk Size
+	//Subchunk Size: 16
 	(header)[16] = 0x10;
 	(header)[17] = 0x00;
 	(header)[18] = 0x00;
 	(header)[19] = 0x00;
 	
 	//AudioFormat
-	(header)[20] = 0x00;
+	(header)[20] = 0x01;
 	(header)[21] = 0x00;
 	
-	//Num Channels
-	(header)[22] = 0x00;
+	//Num Channels: 1
+	(header)[22] = 0x01;
 	(header)[23] = 0x00;
 
-	//AudioFormat: 1(PCM)
-	(header)[24] = 0x01;
-	(header)[25] = 0x00;
-
-	//NumChannels: 1
-	(header)[26] = 0x01;
+	//SampleRate: 16,000
+	(header)[24] = 0x80;
+	(header)[25] = 0x3E;
+	(header)[26] = 0x00;
 	(header)[27] = 0x00;
 
-	//SampleRate: 16,000
-	(header)[28] = 0x80;
-	(header)[29] = 0x3E;
+	//ByteRate: 32,000
+	(header)[28] = 0x00;
+	(header)[29] = 0x7D;
 	(header)[30] = 0x00;
 	(header)[31] = 0x00;
 
-	//ByteRate: 32,000
-	(header)[32] = 0x00;
-	(header)[33] = 0x7D;
-	(header)[34] = 0x00;
-	(header)[35] = 0x00;
-
 	//Block Align
-	(header)[36] = 0x04;
-	(header)[37] = 0x00;
+	(header)[32] = 0x04;
+	(header)[33] = 0x00;
 
 	//BitsPerSample: 16
-	(header)[38] = 0x10;
-	(header)[39] = 0x00;
+	(header)[34] = 0x10;
+	(header)[35] = 0x00;
 
 	//data
-	(header)[40] = 0x64;
-	(header)[41] = 0x61;
-	(header)[42] = 0x74;
-	(header)[43] = 0x61;
+	(header)[36] = 0x64;
+	(header)[37] = 0x61;
+	(header)[38] = 0x74;
+	(header)[39] = 0x61;
 	
 	
-	
+	int i;
+	for(i=0; i <44; i++){
+		if(i%4==0){
+			printf("\n");
+		}
+		printf("%02x ",header[i]);
+	}
 }
 
