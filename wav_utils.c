103218352ad6cbc57e1d64ba92dbf9bc063132f2
Ben <gilmanb33@gmail.com>
2013-02-03 13:06:17 -0500

    Added more to make_header getting closer!
    VLC is almost able to play it!
    "No suitable decoder module
    VLC does not support the audio or video format "undf". Unfortunately
    there is no way for you to fix this."

--- wav_utils.c
+++ wav_utils.c
@@ -41,39 +41,77 @@ void make_header(char* header, int data_length) {
 	memset(header+20, AUDIO_FORMAT, 2);
 	memset(header+22, HEADER_NUM_CHANNELS, 2);
 
-	//AudioFormat: 1(PCM)
-	(header)[20] = 0x01;
+	//Chunk Descriptor
+	(header)[0] = 0x52;
+	(header)[1] = 0x49;
+	(header)[2] = 0x46;
+	(header)[3] = 0x46;
+	
+	//ChunkSize
+	(header)[4] = 0x24;
+	(header)[5] = 0x08;
+	(header)[6] = 0x00;
+	(header)[7] = 0x00;
+	
+	//WAVE
+	(header)[8] = 0x57;
+	(header)[9] = 0x41;
+	(header)[10] = 0x56;
+	(header)[11] = 0x45;
+	
+	//fmt subchunk
+	(header)[12] = 0x66;
+	(header)[13] = 0x6d;
+	(header)[14] = 0x74;
+	(header)[15] = 0x20;
+	
+	//Subchunk Size
+	(header)[16] = 0x10;
+	(header)[17] = 0x00;
+	(header)[18] = 0x00;
+	(header)[19] = 0x00;
+	
+	//AudioFormat
+	(header)[20] = 0x00;
 	(header)[21] = 0x00;
-
-	//NumChannels: 1
-	(header)[22] = 0x01;
+	
+	//Num Channels
+	(header)[22] = 0x00;
 	(header)[23] = 0x00;
 
-	//SampleRate: 16,000
-	(header)[24] = 0x80;
-	(header)[25] = 0x3E;
-	(header)[26] = 0x00;
+	//AudioFormat: 1(PCM)
+	(header)[24] = 0x01;
+	(header)[25] = 0x00;
+
+	//NumChannels: 1
+	(header)[26] = 0x01;
 	(header)[27] = 0x00;
 
-	//ByteRate: 32,000
-	(header)[28] = 0x00;
-	(header)[29] = 0x7D;
+	//SampleRate: 16,000
+	(header)[28] = 0x80;
+	(header)[29] = 0x3E;
 	(header)[30] = 0x00;
 	(header)[31] = 0x00;
 
-	//Block Align
-	(header)[32] = 0x04;
-	(header)[33] = 0x00;
-
-	//BitsPerSample: 16
-	(header)[34] = 0x10;
+	//ByteRate: 32,000
+	(header)[32] = 0x00;
+	(header)[33] = 0x7D;
+	(header)[34] = 0x00;
 	(header)[35] = 0x00;
 
+	//Block Align
+	(header)[36] = 0x04;
+	(header)[37] = 0x00;
+
+	//BitsPerSample: 16
+	(header)[38] = 0x10;
+	(header)[39] = 0x00;
+
 	//data
-	(header)[36] = 0x64;
-	(header)[37] = 0x61;
-	(header)[38] = 0x74;
-	(header)[39] = 0x61;
+	(header)[40] = 0x64;
+	(header)[41] = 0x61;
+	(header)[42] = 0x74;
+	(header)[43] = 0x61;
 	
 	
 	
