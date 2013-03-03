8e65b3355d23d587fadbadbe9673a305d813b8b2
Ben <gilmanb33@gmail.com>
2013-03-03 16:33:00 -0500

    Clustering works, probabilities are wonky
    Doesn't segfault but clusters everything to the same cluster

--- src/dtw/feature.c
+++ src/dtw/feature.c
@@ -8,8 +8,9 @@
 #include "gaussian.h"
 
 void cluster_templates(feature_vectors ** templates,int num_templates){
+	int iterations =0;
 	//Initialize K means cluster by assigning each vector to a cluster
-	printf("initializing clusters...\n");
+	//printf("initializing clusters...\n");
 	int ** cluster_array = (int **)malloc(num_templates*sizeof(int*));
 
 	//Initialize counter for each cluster
@@ -17,7 +18,8 @@ void cluster_templates(feature_vectors ** templates,int num_templates){
 	for(int i=0; i<NUM_CLUSTERS; i++){
 		cluster_count[i] = 0;
 	}
-	
+
+	//Set each point to an initial cluster	
 	for(int i=0; i<num_templates; i++){
 		int num_vectors = templates[i]->num_vectors;	
 		//Holds the cluster that each feature belongs to
@@ -31,78 +33,143 @@ void cluster_templates(feature_vectors ** templates,int num_templates){
 				vector_clusters[j] = NUM_CLUSTERS - 1;
 			}
 			cluster_count[vector_clusters[j]] +=1;
-			printf("%d",  vector_clusters[j]);
 		}
-		printf("\n");
 		cluster_array[i] = vector_clusters;
 	}
 	
 	//Initialize gaussian statistics for each cluster
 	single_gaussian_params ** cluster_stats = (single_gaussian_params**) malloc(NUM_CLUSTERS*sizeof(single_gaussian_params*));
-	print_cluster_count(cluster_count);
+
+	//Create feature_vectors array for each cluster to send off for gaussians
+	feature_vectors * vectors_array = (feature_vectors*) malloc(NUM_CLUSTERS*sizeof(feature_vectors));
+
+	//Keep track of the index in each cluster we are building
+        int * cluster_index = (int*) malloc(NUM_CLUSTERS * sizeof(int));
 
 	//Iterate clustering until convergence
-	//do{
+	do{
+		print_cluster_count(cluster_count);
+		//printf("Zeroing..\n");
+		//Zero out the index for each cluster we will build
+		for(int i=0; i<NUM_CLUSTERS; i++){
+                	cluster_index[i] = 0;
+        	}
+
+		//printf("Initialize feature_vectors\n");
 		//Initialize NUM_CLUSTERS different feature vector arrays to be sent for gaussian evaluation
-		feature_vectors * vectors_array = (feature_vectors*) malloc(NUM_CLUSTERS*sizeof(feature_vectors));
+		//feature_vectors * vectors_array = (feature_vectors*) malloc(NUM_CLUSTERS*sizeof(feature_vectors));
 		for(int i=0; i <NUM_CLUSTERS;i++){
+			if(cluster_count[i]<0){
+				printf("NEGATIVE\n");
+				exit(0);
+			}
 			vectors_array[i].features = (feature*)malloc(sizeof(feature)*cluster_count[i]);
 			vectors_array[i].num_vectors = cluster_count[i];
-			printf("Allocating vector set %d to size: %d\n",i, cluster_count[i]);
 		}
 
-		//Keep track of the index in each cluster we are building
-		int * cluster_index = (int*) malloc(NUM_CLUSTERS * sizeof(int));
-		for(int i=0; i<NUM_CLUSTERS; i++){
-			cluster_index[i] = 0;
-		}
-
+		//printf("Building arrays\n");	
 		//Build arrays of features to pass into gaussian calculation 
 		for(int i=0; i < num_templates; i++){
 			int num_vectors = templates[i]->num_vectors;			
 			for(int j=0; j<num_vectors; j++){
 				int my_cluster = cluster_array[i][j];
-				printf("My Cluster: %d\n",my_cluster);
-				
+			
+					
 				//Feature goes here
-				feature * new_feature = &(vectors_array[i].features[cluster_index[my_cluster]]);
+				feature * new_feature = &(vectors_array[my_cluster].features[cluster_index[my_cluster]]);
+				//Grab feature from here
 				feature * old_feature = &(templates[i]->features[j]);
+/*				
+				printf("New\n");
+				print_feature(new_feature);
+				printf("Old\n");
+				print_feature(old_feature);
+*/				
 				memcpy(new_feature,old_feature,sizeof(feature));
+/*				
+				printf("Copied\n");
+				print_feature(new_feature);
+*/				
+				//new_feature = new_feature;
+				//old_feature = old_feature;
 				
 				//Increment the cluster index to keep track of cluster position
 				cluster_index[my_cluster]+=1;
-				printf("In: %d\n",cluster_index[my_cluster]);
 			}
 		}
 
+		//printf("Gaussian\n");
 		//Get gaussian parameters
 		for(int i =0; i<NUM_CLUSTERS; i++){
-			single_gaussian_params* cluster_gaus_params = compute_single_gaussian_params(&(vectors_array[i]));			
-			print_single_gaussian_params(cluster_gaus_params);
+			single_gaussian_params* cluster_gaus_params;
+			cluster_gaus_params = compute_single_gaussian_params(&(vectors_array[i]));
+			cluster_stats[i] = cluster_gaus_params;			
+//			print_single_gaussian_params(cluster_gaus_params);
 		}
 
+		//printf("Reclassifying\n");
+		//Reevaluate each point given the gaussians
+		int reclassify = 0;
+		for(int i=0; i < num_templates; i++){
+                        int num_vectors = templates[i]->num_vectors;
+                        for(int j=0; j<num_vectors; j++){
+                                int my_cluster = cluster_array[i][j];
+				int new_cluster = my_cluster; //initialize to original cluster
+				double best = 0;
+				feature * point = templates[i]->features;
+				for(int k=0; k<NUM_CLUSTERS; k++){
+					single_gaussian_params * dist = cluster_stats[k];
+					//FIX ME, SHOULD BE A POSITIVE PROB
+					double probability = -single_gaussian_log_pdf(dist,point);
+					if(probability>best){
+						best= probability;
+						new_cluster = k;
+					}
+				}
+				
+				//new_cluster = (my_cluster+1)%NUM_CLUSTERS;
+
+				if(new_cluster!=my_cluster && cluster_count[my_cluster] > 1){
+						reclassify++;	
+						cluster_array[i][j] = new_cluster;
+						cluster_count[my_cluster]-=1;
+						cluster_count[new_cluster]+=1;	
+				}
+					
+			}
+		}
+
+	
+		printf("freeing memory\n");
 		//Free Memory used in recalculating gaussians
-		/*
 		for(int i=0; i <NUM_CLUSTERS;i++){
-			free(feature_set[i]);
+			free(vectors_array[i].features);
 		}
-		free(cluster_index);*/
-		cluster_stats = cluster_stats;
-//	}while(1);	
+		//free(vectors_array);
+		
+		printf("Number reclassified: %d\n",reclassify);
 
-	//Free cluster array
-	/*
+		iterations++;
+	}while(iterations<1000);	
+	printf("done!\n");
+	
+	//Free everything!
 	for(int i=0; i<num_templates; i++){
-		free(vectors_array[i].features);
-	}*/
-	free(vectors_array);
+		free(cluster_array[i]);
+	}	
 	free(cluster_array);
+	free(cluster_count);
+	free(cluster_index);
+
+	//need to return cluster_stats
+	cluster_stats = cluster_stats;
 }
 
 void print_cluster_count(int * cluster_count){
 	for(int i=0; i<NUM_CLUSTERS; i++){
-		printf("%d: %d\n",i,cluster_count[i]);
+		printf("%d: %d\t",i,cluster_count[i]);
 	}	
+	printf("\n");
 }
 
 
