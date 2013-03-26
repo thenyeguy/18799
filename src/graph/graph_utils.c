6f8169a4bb72362a62c45c1cd255081972cb834e
Ben <gilmanb33@gmail.com>
2013-03-26 00:20:20 -0400

    First working graph!
    Check it out.

--- src/graph/graph_utils.c
+++ src/graph/graph_utils.c
@@ -14,47 +14,91 @@ graph * build_graph(char * filename){
 	int start;
 	int end;
 	while (fgets(buffer, buffer_size, file) != NULL){
-		//printf("%s\n",buffer);
 		//FIXME This needs to be done using STRTOK rather than indexing in case double digit
 		if(line_number==0){
 			states = buffer[10]-'0';
-			printf("Number of states: %d\n", states);
+			//printf("Number of states: %d\n", states);
+			grammar_graph = init_graph(states);
 		}
 		else if(line_number==1){
 			start = buffer[13]-'0';
-			printf("Start node: %d\n",start);
+			//printf("Start node: %d\n",start);
 		}
 		else if(line_number==2){
 			end = buffer[17]-'0';
-			printf("Terminal node: %d\n",end);
+			//printf("Terminal node: %d\n",end);
 		}
 		else{
 			int from = buffer[5]-'0';
 			int to = buffer[7]-'0';
 			int hm = buffer[10]-'0';
-			printf("Connect: %d to %d using: %d\n",from,to,hm);
+			connect(grammar_graph,from,to,hm);
+			//printf("Connect: %d to %d using: %d\n",from,to,hm);
 		}
+
 		line_number++;
 	}
-
-
+	print_graph(grammar_graph);
 	fclose(file);
 	return grammar_graph;
 }
 
-/*
-void connect(graph * gr, int from, int to, hidden_markov hmm){
-	graph->nodes[from]	
 
+void connect(graph * gr, int from, int to, int hm){
+	//Generate the new transition with corresponding HMM
+	transition * new_trans = (transition*) malloc(sizeof(transition));
+	new_trans->hmm=NULL; //FIXME should point to the actual HMM
+	new_trans->next_node = &(gr->nodes[to]);
+	new_trans->next_node_index = to;
+	new_trans->next_trans =NULL;
 
+	//Append new transition to the graph_node's linked list of transitions
+	graph_node * from_node = &(gr->nodes[from]);
+	append(from_node,new_trans);
+	from_node->num_transitions++;
 }
-*/
+
+void append(graph_node * from_node, transition * new_trans){
+	transition *temp = from_node->head;
+	if(temp==NULL){
+		from_node->head = new_trans;
+	}
+	else{	
+		while(temp->next_trans){
+			temp = temp->next_trans;
+		}
+		temp->next_trans = new_trans;
+		new_trans->next_trans=NULL;
+	}
+}
+
+void print_graph(graph * gr){
+	for(int i=0; i<gr->num_nodes; i++){
+		printf("Node: %d\n",i);
+		print_graph_node(&(gr->nodes[i]));
+	}
+}
+
+void print_graph_node(graph_node * gn){
+	printf("=======NODE======\n");
+	//printf("num trans: %d\n",gn->num_transitions);
+	transition *temp = gn->head;
+	while(temp){
+		printf("%d with HMM: \n",temp->next_node_index);//FIXME add HMM to graph print out
+		temp = temp->next_trans;
+	}
+	printf("=================\n");
+}
+
 
 graph * init_graph(int num_nodes){
 	graph * grammar_graph = (graph *)malloc(sizeof(graph));
 	graph_node * init_nodes = (graph_node *) malloc(sizeof(graph_node) * num_nodes);
 	grammar_graph->nodes = init_nodes;
 	grammar_graph->num_nodes = num_nodes;
+	for(int i=0; i<num_nodes; i++){
+		grammar_graph->nodes[i].head = NULL;
+	}
 	return grammar_graph;
 }
 
--- src/graph/graph_utils.h
+++ src/graph/graph_utils.h
@@ -17,7 +17,7 @@ typedef struct graph{
  * 		an HMM to another node in the graph.
  */
 typedef struct graph_node{
-	struct transition * transition_list;
+	struct transition * head;
 	int num_transitions;
 }graph_node;
 
@@ -26,7 +26,10 @@ typedef struct graph_node{
  */
 typedef struct transition{
 	struct hidden_markov * hmm;
-	struct graph_node * next;
+	struct graph_node * next_node;
+	struct transition * next_trans;
+	//Used for debugging/displaying structure
+	int next_node_index;
 }transition;
 
 /* hidden_markov	Place holder for the markov model developed previously.
@@ -39,12 +42,42 @@ typedef struct hidden_markov{
 
 }hidden_markov;
 
-
-
-//TODO Comment functions
+/* build_graph	Constructs a grammar graph from the textfile passed in
+ *		After initializing an array of nodes according to the 
+ *		first line in the text file, the build_graph function
+ *		loops through each line specifying an edge and "connects"
+ *		them. 
+ */
 graph * build_graph(char * filename);
+
+/* init_graph	Initializes a graph by allocating an array of nodes. The
+ *		number of nodes corresponds to the first line in the text 
+ *		file.
+ */
 graph * init_graph(int num_nodes);
 
+/* connect	Connect takes the current state of the graph and adds
+ * 		a transition. The 'from' node contains a link list of 
+ *		transitions and connect appends a transition to this 
+ *		The 'hm' parameter must be changed once we decide how 
+ * 		hm's should be held in the graph. I believe there 
+ *		should be an hmm struct somewhere from assignemnt 3?
+ */
+void connect(graph * gr, int from, int to, int hm);
+
+/* append	append takes the current node and adds a new transition
+ *		to its linked list. This indicates a new possible path
+ * 		that can be taken when traversing the graph.
+ */
+void append(graph_node * from_node, transition * new_trans);
+
+/* print_graph	prints out the graph by printing individual nodes
+ */
+void print_graph(graph * gr);
+
+/* print_graph_node	prints a graph node :/
+ */
+void print_graph_node(graph_node * gn);
 
 #endif
 
