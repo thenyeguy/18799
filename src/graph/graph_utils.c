3f30ce4816d3af3f3d6822354249c3e24294e11f
Ben <gilmanb33@gmail.com>
2013-03-25 22:02:30 -0400

    Prints out things that need to be done. Parsing needs to change
    Need to add functions for each of the connection calls.

--- src/graph/graph_utils.c
+++ src/graph/graph_utils.c
@@ -1,7 +1,7 @@
 #include "graph_utils.h"
 
 graph * build_graph(char * filename){
-	graph * grammar_graph = init_graph(4);
+	graph * grammar_graph;
 	FILE * file = fopen(filename,"r");
 	if(NULL==file){
 		printf("Error opening file: %s\n",filename);
@@ -9,14 +9,46 @@ graph * build_graph(char * filename){
 	}
 	int buffer_size = 100;
 	char buffer[buffer_size];
+	int line_number = 0;
+	int states;
+	int start;
+	int end;
 	while (fgets(buffer, buffer_size, file) != NULL){
-		printf("%s\n",buffer);
+		//printf("%s\n",buffer);
+		//FIXME This needs to be done using STRTOK rather than indexing in case double digit
+		if(line_number==0){
+			states = buffer[10]-'0';
+			printf("Number of states: %d\n", states);
+		}
+		else if(line_number==1){
+			start = buffer[13]-'0';
+			printf("Start node: %d\n",start);
+		}
+		else if(line_number==2){
+			end = buffer[17]-'0';
+			printf("Terminal node: %d\n",end);
+		}
+		else{
+			int from = buffer[5]-'0';
+			int to = buffer[7]-'0';
+			int hm = buffer[10]-'0';
+			printf("Connect: %d to %d using: %d\n",from,to,hm);
+		}
+		line_number++;
 	}
 
 
 	fclose(file);
 	return grammar_graph;
 }
+
+/*
+void connect(graph * gr, int from, int to, hidden_markov hmm){
+	graph->nodes[from]	
+
+
+}
+*/
 
 graph * init_graph(int num_nodes){
 	graph * grammar_graph = (graph *)malloc(sizeof(graph));
