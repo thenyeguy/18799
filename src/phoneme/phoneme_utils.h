9630b43b47dacda96b9ffa99a4f842b658c4b9ea
Michael Nye <mnye@andrew.cmu.edu>
2013-04-28 15:03:47 -0400

    Some header changes

--- src/phoneme/phoneme_utils.h
+++ src/phoneme/phoneme_utils.h
@@ -2,18 +2,28 @@
 #define PHONEME_UTILS_H
 
 
-#include "../feature.h"
+#include "../dtw/feature.h"
 
-#define NUM_PHONEMES
-char phoneme_names[NUM_PHONEMES] = {"AX", "AH", "AY", "EH", "EY", "F", "IH",
+
+/* Encapsulate a set of recordings for a digit
+ */
+typedef struct {
+    char name[28];
+    int num_recordings;
+    feature_vectors** recordings;
+} digit_recordings;
+
+
+#define NUM_PHONEMES 19
+char* phoneme_names[NUM_PHONEMES] = {"AX", "AH", "AY", "EH", "EY", "F", "IH",
         "IY", "K", "N", "OW", "R", "S", "T", "TH", "UW", "V", "W", "Z"};
 
 /* get_phoneme_initial_models - given a set of recordings of digits, splits
  *     those digits into phonemes and trains an initial set of gaussian clusters
  *     for those phonemes
  */
-gaussian_cluster** get_phoneme_initial_models(feature_vectors** recordings,
-                                              int num_recordings);
+gaussian_cluster** get_phoneme_initial_models(digit_recordings** recordings,
+                                              int num_digits);
 
 
 #endif
