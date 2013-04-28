0422692518d102e3e2f2405b0038885e9bdbfa43
Michael Nye <mnye@andrew.cmu.edu>
2013-04-28 14:37:24 -0400

    Intial header file for phoneme splitting

---
+++ src/phoneme/phoneme_utils.h
@@ -0,0 +1,18 @@
+#ifndef PHONEME_UTILS_H
+#define PHONEME_UTILS_H
+
+
+#include "../feature.h"
+
+char phoneme_names[19] = {"AX", "AH", "AY", "EH", "EY", "F", "IH", "IY", "K",
+                          "N", "OW", "R", "S", "T", "TH", "UW", "V", "W", "Z"};
+
+/* get_phoneme_initial_models - given a set of recordings of digits, splits
+ *     those digits into phonemes and trains an initial set of gaussian clusters
+ *     for those phonemes
+ */
+gaussian_cluster** get_phoneme_initial_models(feature_vectors** recordings,
+                                              int num_recordings);
+
+
+#endif
