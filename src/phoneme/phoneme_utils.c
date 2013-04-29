3cbf94b6f078cf48cf571708f113e6e0b5cf7ad0
Emily Grove <egrove@andrew.cmu.edu>
2013-04-29 12:51:28 -0400

    removed fixme in phoneme_utils, proofread get_phoneme_initial_models, made some minor formatting changes

--- src/phoneme/phoneme_utils.c
+++ src/phoneme/phoneme_utils.c
@@ -27,73 +27,76 @@ char** word_phonemes[NUM_WORDS] = {zero,one,two,three,four,five,six,seven,eight,
 
 
 
-gaussian_cluster** get_phoneme_initial_models(recording_set** recordings,int num_digits){
+gaussian_cluster** get_phoneme_initial_models(recording_set** recordings,int num_digits) {
 	
-	//Initialize a count of the number of feature vectors needed for each phoneme
+	// Initialize a count of the number of feature vectors needed for each phoneme
 	int feature_vectors_per_phoneme[NUM_PHONEMES];
-	for(int i=0; i<NUM_PHONEMES; i++){
+	for (int i=0; i<NUM_PHONEMES; i++) {
 		feature_vectors_per_phoneme[i] = 0;
 	}
 
-	//Loop through each digit in our array of recordings
-	for(int i=0; i<num_digits; i++){
+	// Loop through each digit in our array of recordings
+	for (int i=0; i<num_digits; i++) {
 		
-		//Get list of phonemes in that digit
+		// Get list of phonemes in that digit
 		char * digit_name = recordings[i]->name;
 		int digit_index = wordToModelIndex(digit_name);
 		char ** digit_phonemes = word_phonemes[digit_index];
-		int num_phonemes = phonemes_in_word[digit_index];
+		int num_phonemes_in_digit = phonemes_in_word[digit_index];
 		
-		//For each recording in our recording set, increment that phoneme count
-		for(int j=0; j<num_phonemes; j++){
+		// For each recording in our recording set, increment that phoneme count
+		for (int j=0; j<num_phonemes_in_digit; j++) {
 			int phoneme_index = phonemeToModelIndex(digit_phonemes[j]);
 			feature_vectors_per_phoneme[phoneme_index]++;
 		}
 	}	
 
-	//Initialize array of feature vector arrays to ship off for clustering
+	// Initialize array of feature vector arrays to ship off for clustering
 	feature_vectors** feature_vectors_for_phonemes[NUM_PHONEMES];
 
-	//Initializes the array of feature vector arrays for each phoneme to fill in by splitting
-	for(int i=0; i<NUM_PHONEMES; i++){
-		feature_vectors_for_phonemes[i] = (feature_vectors**)malloc(sizeof(feature_vectors*) * 
-			feature_vectors_per_phoneme[i]);
+	// Initializes the array of feature vector arrays for each phoneme to fill in by splitting
+	for (int i=0; i<NUM_PHONEMES; i++) {
+		feature_vectors_for_phonemes[i] = 
+			(feature_vectors**)malloc(sizeof(feature_vectors*) * 
+									feature_vectors_per_phoneme[i]);
 	}
 
-	//Initialize an array of indexes into the Phoneme array to keep track of which feature_vector we are on
+	// Initialize an array of indexes into the Phoneme array to keep track of which 
+	// feature_vector we are on
 	int phoneme_feature_vector_index[NUM_PHONEMES];
-	for(int i=0; i<NUM_PHONEMES; i++){
+	for (int i=0; i<NUM_PHONEMES; i++) {
 		phoneme_feature_vector_index[i]=0;
 	}
 
-	//Loop through each digit
-	for(int i=0; i<num_digits; i++){
+	// Loop through each digit
+	for (int i=0; i<num_digits; i++) {
 		
-		//Get the word, number of phonemes, and list of phonemes for each digit
+		// Get the word, number of phonemes, and list of phonemes for each digit
 		int word_index = wordToModelIndex(recordings[i]->name);
 		int num_phonemes = phonemes_in_word[word_index];
 		char** phonemes = word_phonemes[word_index];
 
-		//Loop through each recording in the digit's recording set
-		for(int j=0; j< (recordings[i]->num_recordings) ; j++){
+		// Loop through each recording in the digit's recording set
+		for (int j=0; j<(recordings[i]->num_recordings); j++) {
 		
-			//Split the recording into num_phoneme different phoneme slices
-			feature_vectors** split = NULL; //SPLIT (recordings[i]->recordings[j],num_phonemes) FIXME EMILY!
+			// Split the recording into num_phoneme different phoneme slices
+			feature_vectors** split = 
+				split_feature_vectors(recordings[i]->recordings[j], num_phonemes);
 
-			//Add the newly split phoneme into the phoneme feature vector array
+			// Add the newly split phoneme into the phoneme feature vector array
 			for(int k=0; k<num_phonemes; k++){
 				
-				//Figure out which phoneme this feature_vector is a member of
+				// Figure out which phoneme this feature_vector is a member of
 				char *phoneme_name = phonemes[k];
 				int phoneme_index = phonemeToModelIndex(phoneme_name);
 			
-				//Figure out which feature vector within the phoneme array to put it	
+				// Figure out which feature vector within the phoneme array to put it	
 				int feature_vector_index = phoneme_feature_vector_index[phoneme_index];
 				
-				//Actually set the feature vector to point to that slice of the recording
+				// Actually set the feature vector to point to that slice of the recording
 				feature_vectors_for_phonemes[phoneme_index][feature_vector_index] = split[k];
 
-				//Increment the feature vector index for that phoneme
+				// Increment the feature vector index for that phoneme
 				phoneme_feature_vector_index[phoneme_index] ++;
 			}			
 		}
@@ -106,7 +109,8 @@ gaussian_cluster** get_phoneme_initial_models(recording_set** recordings,int num
 	 * indexes to get a gaussian params for each phoneme */
 
 
-	gaussian_cluster ** phoneme_clusters = (gaussian_cluster **) malloc(sizeof(gaussian_cluster*)*NUM_PHONEMES);
+	gaussian_cluster ** phoneme_clusters = 
+		(gaussian_cluster **) malloc(sizeof(gaussian_cluster*)*NUM_PHONEMES);
 	for(int i=0; i<NUM_PHONEMES; i++){
 
 		phoneme_clusters[i] = cluster_templates(feature_vectors_for_phonemes[i],
